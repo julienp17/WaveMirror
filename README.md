8045eb8b022376588d6ee06dab91cead94a1e4bf
Julien <julien.pause17@gmail.com>
2018-12-14 15:04:07 +0400

    Update

--- README.md
+++
@@ -1,5 +0,0 @@
-# WaveMirror
-
-This repository is for an Highschool project in Lycée George Brassens (St-Denis, Réunion, France). It is based on [Daniel Rozin's Weave Mirror](https://smoothware.com/danny/weavemirror.html) with a 12x10 grid of servo-motors. 
-
-This project help the students develop with interesting software and technologies such as Arduino, Raspberry PI, SolidWorks or Scilab.
---
+++ arduino/servos/servoSeul/servoSeul.ino
@@ -0,0 +1,37 @@
+int periode = 20000; 
+int pinServo = 8; 
+int tempsTransition = 4;
+
+void setup() {
+  pinMode(pinServo, OUTPUT); 
+  digitalWrite(pinServo, LOW); 
+}
+
+void loop() {
+  
+  for (int angle = 0; angle <= 180; angle += 1) { 
+    setAngle(angle);
+    delay(tempsTransition);
+  }
+  
+  for (int angle = 0; angle <= 180; angle += 1) { 
+    setAngle(180 - angle); 
+    delay(tempsTransition);
+  }
+}
+
+void setAngle(int a) {
+
+  int duree = map(a, 0, 180, 500, 2600); // on transforme l'angle en microsecondes et on stocke dans la variable duree (180, 545, 2500)
+
+  digitalWrite(pinServo, LOW); //on met le pin à l'état bas
+
+  digitalWrite(pinServo, HIGH); // on envoie l'impulsion
+
+  delayMicroseconds(duree); // pendant la bonne durée
+
+  digitalWrite(pinServo, LOW); // on stoppe l'impulsion
+
+  delayMicroseconds(periode - duree); // on attend le temps restant pour atteindre la période
+
+}
