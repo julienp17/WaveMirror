fa6979b22fe8a9e183d2b235ce97d66020d1b9e8
Julien <julien.pause17@gmail.com>
2018-12-15 09:13:18 +0400

    Add 800x600 grid image for reference

--- raspberryPi/smiley.py
+++ raspberryPi/smiley.py
@@ -1,29 +1,35 @@
 # -*-coding:Utf-8 -*
 
+# This programm shows 2 differents images every 3 seconds,
+# in this case a happy and a sad smiley,  in a infinite loop.
+# It displays their grayscale values and serve as eye-candy
+# for the day of our presentation. 
+
 import psutil
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from PIL.Image import *
 from time import sleep
 
+fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
 hauteur = 0
 longeur = 0
 divH = 10
 divL = 12
 happy = True
 
-for x in range(2):
+while True:
 	if happy:
-		smiley = "smileyHappy.png"
+		#smiley = "pictures/smileyHappy.png"
+		smiley = "pictures/grid.png"
 		happy = False
 	else:
-		smiley = "smileySad.png"
+		smiley = "pictures/smileySad.png"
 		happy = True
-	image = open(smiley)
-	image = image.convert('L')
+	image = open(smiley).convert('L')
 	(l, h) = image.size
 	tailleH = int(h/divH)
 	tailleL = int(l/divL)
-	print(tailleH, tailleL)
+	draw = ImageDraw.Draw(image)
 
 	for hauteur in range(divH):
 		pixHDepart = tailleH * hauteur
@@ -47,7 +53,13 @@ for x in range(2):
 					a += 1
 
 			moyValPix = int(sommeValPix/a)
-			sleep(0.001)
+			draw.rectangle(((pixLDepart, pixHDepart), (pixLFinal, pixHFinal)), outline="red")
+			if moyValPix > 165 :
+				filou = "black"  
+			else:
+				filou = "white"
+			draw.text(((pixLDepart+pixLFinal)/2, (pixHDepart+pixHFinal)/2), str(moyValPix), fill=filou, font=fnt)
+			#sleep(0.001)
 	image.show()
 	sleep(3)
 	for proc in psutil.process_iter():
