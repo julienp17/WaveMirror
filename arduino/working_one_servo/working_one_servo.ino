d643344ab5945c474010652d727c6faad5ae00c7
Julien <julien.pause17@gmail.com>
2019-04-20 15:34:10 +0400

    Clean working one servo code

--- arduino/working_one_servo/working_one_servo.ino
+++ arduino/working_one_servo/working_one_servo.ino
@@ -12,46 +12,22 @@
 
 #define SLAVE_ADDRESS 0x03
 
-Servo servo_deux;  
-Servo servo_trois;
-Servo servo_quatre;
-Servo servo_cinq;
-Servo servo_six;
-Servo servo_sept;
-Servo servo_huit;  
-Servo servo_neuf;
-
-int received = 0;
+Servo myservo;
 
 void setup() {
-  servo_deux.attach(2);  
-  servo_trois.attach(3);
-  servo_quatre.attach(4);  
-  servo_cinq.attach(5);
-  servo_six.attach(6);  
-  servo_sept.attach(7);
-  servo_huit.attach(8);  
-  servo_neuf.attach(9);
+  myservo.attach(2);
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveData);
 }
 
 void loop() {
-  delay(100);                      // waits for the servo to get there
+  delay(100); // waits for the servo to get there
 }
 
 void receiveData(int byteCount) {
   while(Wire.available()) {
     int color = Wire.read();
     int angle = map(color, 0, 255, 180, 0);
-    received++;
-  }
-    if (pouf == 0){
-      servo_deux.write(angle);
-      pouf++;
-  } else {
-      servo_trois.write(angle);
-      pouf--;
-  }
+    myservo.write(angle);
   }
 }
